dc5fcdcf35b84d6029394f184a6d88207b9046f4
lifuyuan123 <446960231@qq.com>
2023-02-22 16:25:35 +0800

    微调

--- app/src/main/java/com/lfy/test1/MainActivity.kt
+++ app/src/main/java/com/lfy/test1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.lfy.test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lfy.mylibrary.ToastUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToastUtils.init(applicationContext)
+        window.decorView.postDelayed({
+            ToastUtils.toast("成功")
+        },2000)
+
     }
 }
